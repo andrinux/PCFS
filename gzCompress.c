b4740e96da166b9991e851fe5fecab35942dc7ab
iAndrinux <andrinux.loid@gmail.com>
2015-06-04 12:03:51 -0700

    compress data from fd_src to fd_des

--- gzCompress.c
+++ gzCompress.c
@@ -10,7 +10,8 @@
 
 #define BUF_SIZE 4096
 
-
+#include "file.h"
+#include "log.h"
 
 /**
  * Close compressed stream.
@@ -35,4 +36,63 @@ static int gzClose(gzFile fd_gz)
 		return -1;
 	}
 	return 0;
+}
+
+
+
+/**
+ * Compress data from fd_source into fd_dest.
+ *
+ * @param fd_source	Source file descriptor
+ * @param fd_dest	Destination file descriptor
+ * @return 		Number of bytes readed from fd_source or -1 on error
+ */
+static off_t gzCompress(void *cancel_cookie, int fd_source, int fd_dest)
+{
+	char buf[BUF_SIZE];
+	int wr;
+	int rd;
+	gzFile fd_gz = NULL;
+	off_t size = 0;
+	int dup_fd;
+
+	dup_fd = dup(fd_dest);
+	if (dup_fd == -1)
+	{
+		return (off_t) FAIL;
+	}
+
+	fd_gz = gzdopen(dup_fd, COMPRESSLEVEL_BACKGROUND);
+	if (fd_gz == NULL)
+	{
+		file_close(&dup_fd);
+		return (off_t) FAIL;
+	}
+
+	while ((rd = read(fd_source, buf, sizeof(buf))) > 0)
+	{
+		size += rd;
+
+		wr = gzwrite(fd_gz, buf, rd);
+		if(wr == -1)
+		{
+			gzClose(fd_gz);
+			return (off_t) FAIL;
+		}
+
+		if (compress_testcancel(cancel_cookie))
+		{
+			break;
+		}
+	}
+
+	if (rd < 0)
+	{
+		gzClose(fd_gz);
+		return (off_t) FAIL;
+	}
+	
+	gzClose(fd_gz);
+	
+	return size;
 }
